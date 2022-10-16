1c49781fc2d0c76a354928407ee188668c540009
WariaX <115689237+WariaX@users.noreply.github.com>
2022-10-17 00:55:25 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,12 @@
 def cross(n):
-    pass
+    for i in range(1 , (3 * n)+1):
+        if i <= n:
+            print((n * " ") + (n * "X") + (n * " "))      
+    
+        elif i > (n + 1) and i <= (2 * n):            
+            print ((n * 3) * "X")
+
+        elif i > (2 * n):
+            print((n * " ") + (n * "X") + (n * " "))
+
+cross(4)
