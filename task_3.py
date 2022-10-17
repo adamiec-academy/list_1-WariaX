e756ea5c5733a33119b4a0c6889a875188a063ff
WariaX <115689237+WariaX@users.noreply.github.com>
2022-10-17 12:42:57 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,2 +1,12 @@
-def envelope(n):
-    pass
+def envelope(n): 
+    for cheat1 in range(0, n):
+        for cheat2 in range(0, n):
+            if (cheat1 == 0 or cheat1 == n - 1
+                or cheat2 == 0 or cheat2 == n - 1
+                or cheat1 == cheat2 or cheat1 == n - 1 - cheat2):        
+                print( "*", end="")
+ 
+            else:
+                print( " ",end="")
+        print("")
+envelope(10)
