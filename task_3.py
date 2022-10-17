7ae469a6d50f1612e6b926776c8274e0c37808ad
WariaX <115689237+WariaX@users.noreply.github.com>
2022-10-17 12:51:47 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,9 +1,9 @@
 def envelope(n): 
-    for cheat1 in range(0, n):
-        for cheat2 in range(0, n):
-            if (cheat1 == 0 or cheat1 == n - 1
-                or cheat2 == 0 or cheat2 == n - 1
-                or cheat1 == cheat2 or cheat1 == n - 1 - cheat2):        
+    for cheat1 in range(0, (2*n+1)):
+        for cheat2 in range(0, (2*n+1)):
+            if (cheat1 == 0 or cheat1 == (2*n+1) - 1
+                or cheat2 == 0 or cheat2 == (2*n+1) - 1
+                or cheat1 == cheat2 or cheat1 == (2*n+1) - 1 - cheat2):        
                 print( "*", end="")
  
             else:
