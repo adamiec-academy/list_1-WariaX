b08fa0dbbe5e9d921a490e96c2f148e99164c169
WariaX <115689237+WariaX@users.noreply.github.com>
2022-10-17 10:45:22 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,20 +1,13 @@
 def chess_board(n, k): 
-
     for fortheemperor in range(n):
-
 
         for deus in range(k): 
             for vult in range (n):        
-                print((" " * k ) + ("#" * k ) , end="" )
-                
-            print() 
-
+                print((" " * k ) + ("#" * k ), end="")                
+            
         for satan in range(k): 
             for vult in range (n):        
-                print(("#" * k ) + (" " * k ) , end="" )
-                
-            print() 
-    print()        
-     
-
+                print(("#" * k ) + (" " * k ), end="")
+                                   
+  
 chess_board(4, 2)
