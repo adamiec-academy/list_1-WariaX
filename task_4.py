0b3e613c6f2c2f13321bac00e9e38fe6badd7ace
WariaX <115689237+WariaX@users.noreply.github.com>
2022-10-19 08:21:01 +0200

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,5 +1,4 @@
-def snowman (n):    
-    def snowball(n, k):
+def snowball(n, k):
         promien = (n / 2) - 0.5
         for i in range(n):
             print(2 * k * " ", end="")
@@ -14,34 +13,9 @@ def snowman (n):
             
             print()
 
-        promien2 = ((n+2) / 2) - 0.5
-        for i in range(n+2):
-            print(k * " ", end="")
-            for j in range(n+2):
-                x = i - promien2
-                y = j - promien2
-                if x * x + y * y <= promien2 * promien2 + 1:
-                    print('#', end='  ')
-                    
-                else:
-                    print(' ', end='  ')
-            
-            print()    
+
+snowball(7, 2)   
 
 
-        promien3 = ((n+4) / 2) - 0.5
-        for i in range(n+4):
-            for j in range(n+4):
-                x = i - promien3
-                y = j - promien3
-                if x * x + y * y <= promien3 * promien3 + 1:
-                    print('#', end='  ')
-                    
-                else:
-                    print(' ', end='  ')
-            
-            print() 
-
-
-    snowball(n, n-4)
-snowman (7)
+def snowman(n)
+    pass
